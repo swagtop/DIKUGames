50dc56632d94c37fc9201cf4d45a1d21ddb55cba
thedb <tcj841@alumni.ku.dk>
2024-03-24 22:45:02 +0100

    This is a commit
    Co-authored-by: name <Janus984@email.com>

--- Breakout/Game.cs
+++ Breakout/Game.cs
@@ -4,18 +4,22 @@ using DIKUArcade.Events;
 using DIKUArcade.Input;
 
 namespace Breakout;
-public class Game : DIKUGame {
+public class Game : DIKUGame {  
     private GameEventBus eventBus = BreakoutBus.GetBus();
+    // TODO: Tilføj stateMachine
     public Game(WindowArgs windowArgs) : base(windowArgs) {
         window.SetKeyEventHandler(KeyHandler);
     }
 
     public override void Render() { 
+        // TODO: Tilføj rendering fra den aktive state i stateMachine
     }
 
     public override void Update() {
+        // TODO: Tilføj updates af den aktive state i stateMachine
     }
 
     private void KeyHandler(KeyboardAction action, KeyboardKey key) {
+        // TODO: Send keyhandling til den aktive state i stateMachine
     }
 }
