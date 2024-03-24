5d5f30d08e3ae729fdacb09be9b91578b7b257cd
thedb <tcj841@alumni.ku.dk>
2024-03-24 10:03:12 +0100

    added indentation

--- Breakout/Program.cs
+++ Breakout/Program.cs
@@ -4,9 +4,9 @@ using DIKUArcade.GUI;
 namespace Breakout; 
 public class Program {
     static void Main(string[] args) {
-    var windowArgs = new WindowArgs() { Title = "Breakout v0.1" };
-    var game = new Game(windowArgs);
-    game.Run();
-    Console.WriteLine("Hello World!"); // TODO: Delete this line!
+        var windowArgs = new WindowArgs() { Title = "Breakout v0.1" };
+        var game = new Game(windowArgs);
+        game.Run();
+        Console.WriteLine("Hello World!"); // TODO: Delete this line!
     }
 }
