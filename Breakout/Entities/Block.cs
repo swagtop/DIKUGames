c721c10d9afa17c21a753d952298dd27a25a5a14
thedb <tcj841@alumni.ku.dk>
2024-04-02 10:23:28 +0200

    added unbreakable bool to block class

--- Breakout/Entities/Block.cs
+++ Breakout/Entities/Block.cs
@@ -6,19 +6,22 @@ using DIKUArcade.Graphics;
 namespace Breakout.Entities;
 public class Block : Entity {
     private IBaseImage damagedImage;
+    private bool unbreakable;
     private int maxHitpoints;
     private int hitpoints;
     public int HitPoints {
         get => hitpoints;
         set {
+            if (unbreakable) { return; }
             if (value == maxHitpoints/2) { Image = damagedImage; } 
             if (value > 0) { hitpoints = value; }
             else { this.DeleteEntity(); }
         }
     }
-    public Block(int startHitpoints, IBaseImage normal, IBaseImage damaged, StationaryShape shape) : base(shape, normal) {
+    public Block(int maxHp, IBaseImage normal, IBaseImage damaged, StationaryShape shape, bool unbreakable=false) : base(shape, normal) {
         damagedImage = damaged;
-        maxHitpoints = startHitpoints;
-        hitpoints = maxHitpoints;
+        unbreakable = unbreakable;
+        maxHitpoints = maxHp;
+        hitpoints = maxHp;
     }
 }
