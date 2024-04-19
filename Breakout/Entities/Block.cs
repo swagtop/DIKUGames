faf6902b2a02e142d8fb5662b9004df3de62cb10
thedb <tcjj841@alumni.ku.dk>
2024-04-19 10:41:16 +0200

    fixed damagedimage logic

--- Breakout/Entities/Block.cs
+++ Breakout/Entities/Block.cs
@@ -14,7 +14,7 @@ public class Block : Entity {
         get => hitpoints;
         set {
             if (unbreakable) { return; } // Hitpoints cannot be set if block is unbreakable.
-            if (value >= maxHitpoints/2 && hitpoints < maxHitpoints/2) { Image = damagedImage; }
+            if (value <= maxHitpoints/2 && hitpoints > maxHitpoints/2) { Image = damagedImage; }
             if (value > 1) { hitpoints = value; }
             else { this.DeleteEntity(); }
         }
