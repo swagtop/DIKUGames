6d94b238047210f11d3d259195b5c62b3b0b2077
thedb <tcjj841@alumni.ku.dk>
2024-04-24 22:32:02 +0200

    breakout is now version 1!!!

--- Breakout/Program.cs
+++ Breakout/Program.cs
@@ -4,7 +4,7 @@ using DIKUArcade.GUI;
 namespace Breakout; 
 public class Program {
     static void Main(string[] args) {
-        var windowArgs = new WindowArgs() { Title = "Breakout v0.1" };
+        var windowArgs = new WindowArgs() { Title = "Breakout v1" };
         var game = new Game(windowArgs);
         game.Run();
     }
