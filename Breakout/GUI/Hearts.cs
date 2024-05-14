785e3fe9977c174cd8931f7437ced7a22639b259
thedb <tcj841@alumni.ku.dk>
2024-05-14 22:40:12 +0200

    hearts rendering optimization cannot figure out why there is space between hearts for now

--- Breakout/GUI/Hearts.cs
+++ Breakout/GUI/Hearts.cs
@@ -8,6 +8,10 @@ public class Hearts {
     private uint startAmount;
     private IBaseImage image;
     private IBaseImage emptyImage;
+    private Shape renderShape = new StationaryShape(
+        new Vec2F(0.777f, 0.3f),
+        new Vec2F(0.05f, 0.05f)
+    );
     public uint Amount;
 
     public Hearts(uint startAmount, IBaseImage image, IBaseImage emptyImage) {
@@ -23,18 +27,15 @@ public class Hearts {
     }
 
     public void RenderHearts() {
+        float originalX = renderShape.Position.X;
         for (int i = 0; i < startAmount; i++) {
+            renderShape.Position.X += (float)i * 0.05f;
             if (i < Amount) {
-                image.Render(new StationaryShape(
-                    new Vec2F(0.777f + (float)i * 0.05f, 0.3f), 
-                    new Vec2F(0.05f, 0.05f)
-                ));
+                image.Render(renderShape);
             } else {
-                emptyImage.Render(new StationaryShape(
-                    new Vec2F(0.777f + (float)i * 0.05f, 0.3f), 
-                    new Vec2F(0.05f, 0.05f)
-                ));
+                emptyImage.Render(renderShape);
             }
         }
+        renderShape.Position.X = originalX;
     }
 }
