b04a2215d3d22e7de8a165fd48c8307a23a0bdd1
bfl942 <145555902+bfl942@users.noreply.github.com>
2024-05-22 11:45:00 +0200

    PowerUpEffects

--- Breakout/PowerupEffects/DoubleSize.cs
+++ Breakout/PowerupEffects/DoubleSize.cs
@@ -1,5 +1,10 @@
 namespace Breakout.PowerUpEffects;
 
 public class DoubleSize : PowerUpEffects {
-
+    public vois ActivateDoubleSize (List<Ball>) {
+        foreach (var ball in balls) {
+            ball.Shape.Extent *= 2;
+        }
+        // Implementering af timer 
+    }
 }
