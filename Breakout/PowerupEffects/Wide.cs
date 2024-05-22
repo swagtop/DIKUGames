2c78798546550f9ea1a9cd362dd17c19723518c2
bfl942 <145555902+bfl942@users.noreply.github.com>
2024-05-22 11:44:38 +0200

    PowerUpEffects

--- Breakout/PowerupEffects/Wide.cs
+++ Breakout/PowerupEffects/Wide.cs
@@ -1,5 +1,11 @@
 namespace Breakout.PowerUpEffects;
 
 public class Wide : PowerUpEffects {
+    public void ActivateWide () {
+        var currentPosition = this.Shape.Position;
+        this.Shape.Exstent = new Vec2F(this.SHape.Extent.X * 2, this.Shape.Extent.Y);
+        this.Shape.Position = new.Vec2F(currentPosition.X - (this.Shape.Extent.X / 4), currentPosition.Y);
+        // Implementering af timer 
+    }
 
 }
