c0ab20c0f7bafd4aef8d4483136ebef96f3120a1
bfl942 <145555902+bfl942@users.noreply.github.com>
2024-05-22 11:44:09 +0200

    PowerUpEffects

--- Breakout/PowerupEffects/Split.cs
+++ Breakout/PowerupEffects/Split.cs
@@ -1,7 +1,17 @@
 namespace Breakout.PowerUpEffects;
 
 public class Split : PowerUpEffects {
-    public void (Player player) {
-        player.SplitBalls();
+    public void ActivateSplit(List<Ball> balls) {
+        var newBalls = new List<Ball>();
+        foreach (var ball in balls) {
+            newBalls.Add(CreateNewBall(ball, new Vec2F(1, 1)));
+            newBalls.Add(CreateNewBall(ball, new Vec2F(-1, 1)));
+            newBalls.Add(CreateNewBall(ball, new Vec2F(0, 1)));
+        }
+        balls.AddRange(newBalls);  
     }
+    private Ball CreateNewBall(Ball originalBall, Vec2F direction) {
+        var newBall = new Ball(new DynamicShape(originalBall.Shape.Position, originalBall.Shape.Extent, direction), Image);
+    }
+    return newBall;
 }
