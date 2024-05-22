0f9089de1459f6d313995a2658bc4ced89d6ad41
thedb <tcj841@alumni.ku.dk>
2024-05-22 22:08:31 +0200

    more changing around

--- Breakout/GUI/Background.cs
+++ Breakout/GUI/Background.cs
@@ -6,10 +6,11 @@ using DIKUArcade.Math;
 
 public class Background {
     private IBaseImage image;
-    private Shape shape = new StationaryShape(new Vec2F(0.0f, 0.0f), new Vec2F(1.0f, 1.0f));
+    private Shape shape;
 
     public Background(IBaseImage image) {
         this.image = image;
+        this.shape = new StationaryShape(new Vec2F(0.0f, 0.0f), new Vec2F(1.0f, 1.0f));
     }
 
     public void RenderBackground() {
