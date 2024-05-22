9daf0cba4b339812a4796800c42dc8a510d74c09
bfl942 <145555902+bfl942@users.noreply.github.com>
2024-05-22 11:46:36 +0200

    PowerUpEffects

--- Breakout/Entities/PowerupBlock.cs
+++ Breakout/Entities/PowerupBlock.cs
@@ -7,4 +7,19 @@ namespace Breakout.Entities;
 public class PowerUpBlock : Block{
     public PowerUpBlock(IBaseImage image, IBaseImage damagedImage, Shape shape) : base(image, damagedImage, shape){
     }
+
+    public void ActivatePowerUp (PowerUpType powerUpType) {
+        switch (powerUpType) {
+            case PowerUpType.PlayerSpeed:
+                break;
+            case PowerUpType.Wide:
+                break;
+            case PowerUpType.DoubleSize:
+                break;
+            case PowerUpType.Split:
+                break;
+            case PowerUpType.HardBall:
+                break;
+        }
+    }
 }
